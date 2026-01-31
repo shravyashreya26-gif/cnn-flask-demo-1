0949522f55be91bb85f1968575e39e49f56a9aab
shravyashreya26-gif <shravyashreya26@gmail.com>
2026-01-31 14:46:29 +0530

    do

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ def hello_world():
     return render_template("index.html")
 @app.route("/<name>")
 def name(name):
-    return render_template("name.html", name=name)
+    return render_template("name.html", name=name, image=name+".jpg")
 @app.route("/abc")
 def abc():
     return render_template("abc.html")
     
     
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True , host="0.0.0.0", port=5000)
